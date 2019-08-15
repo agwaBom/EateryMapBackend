95ae635e9543af34bcfdf3a3ee6a30aa28d54385
HyunJin Kim <hyunjinkim@HyunJinui-MacBook-Pro.local>
2019-08-15 19:10:19 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import mongoose from 'mongoose';
+import {
+  facebookLogin,
+  facebookMiddleware,
+  googleLogin,
+  googleMiddleware,
+  oauthCallback,
+} from './controllers/auth';
+import { list, create } from './controllers/comments';
+
+// Connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/react-native-comments');
+
+// Initialize http server
+const app = express();
+app.use(bodyParser.json());
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set up auth routes
+app.get('/auth/facebook', facebookLogin);
+app.get('/auth/google', googleLogin);
+app.get('/auth/facebook/callback', facebookMiddleware, oauthCallback);
+app.get('/auth/google/callback', googleMiddleware, oauthCallback);
+
+// Set up comment routes
+app.route('/comments')
+  .get(list)
+  .put(create);
+
+// Launch the server on the port 3000
+const server = app.listen(3000, () => {
+  const { address, port } = server.address();
+  console.log(`Listening at http://${address}:${port}`);
+});
